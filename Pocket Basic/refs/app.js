158334fdbdc30469d63db61974fcb13086a58dae
PedroHSMoreira <moreiraphs@outlook.com>
2019-09-02 19:36:36 -0300

    ADD.: Pocket Basic MOD.: delete index.html /refs

--- Pocket Basic/refs/app.js
+++ Pocket Basic/refs/app.js
@@ -1,13 +1,14 @@
 ﻿// Models da aplicação
 class Expense {
-  constructor(id, day, type, description, val) {
+  constructor(id, day, type, description, val, filtro) {
       this.id = id,
       //this.year = year,
      // this.month = month,
       this.day = day,
       this.type = type,
       this.description = description,
-      this.val = val
+      this.val = val,
+      this.filtro = filtro
   }
   // Validando se todos os campos foram preenchidos
   validator() {
@@ -106,7 +107,7 @@ class DB {
   }
 
   update(_id, // _year, _month,
-   _day, _type, _description, _val) {
+   _day, _type, _description, _val, _filtro) {
     let expenseToUpdate = this.getExpense(_id)
     let {
       id,
@@ -116,6 +117,7 @@ class DB {
       type,
       description,
       val,
+      filtro,
     } = expenseToUpdate
     // Compara se os valores foram alterados
       // year = year == _year ? year : _year
@@ -124,7 +126,7 @@ class DB {
       type = type == _type ? type : _type
       description = description == _description ? description : _description
       val = val == _val ? val : _val
-
+      filtro = filtro == _filtro ? filtro : _filtro
     let expense = {
       id,
     // year,
@@ -132,7 +134,8 @@ class DB {
       day,
       type,
       description,
-      val
+      val,
+      filtro
     }
     localStorage.setItem(`${id}`, JSON.stringify(expense))
   }
@@ -151,6 +154,7 @@ function registerExpense() {
   let type = document.querySelector('#type')
   let description = document.querySelector('#description')
   let val = document.querySelector('#val')
+  let filtro = document.querySelector('#filtro')
 
   let expense = new Expense(
     db.getId(),
@@ -159,7 +163,8 @@ function registerExpense() {
     day.value,
     type.value,
     description.value,
-    val.value
+    val.value,
+    filtro.value
   )
 
   if (expense.validator()) {
@@ -197,7 +202,7 @@ function messageAlert(type, message) {
   alert.textContent = message
 }
 // Função que mostra a tabela das despesas
-function getList(expense = [], filtred = false) {
+function getListDespesas(expense = [], filtred = false) {
   let tableExpenses = document.querySelector('#tableExpenses')
   tableExpenses.innerHTML = '' // Limpando a tabela
 
@@ -209,6 +214,8 @@ function getList(expense = [], filtred = false) {
   }
 
   expense.forEach(el => {
+    // Testa se cada elemento campo filtro = "Despesa"
+    if(el.filtro == "despesa"){
     let row = tableExpenses.insertRow() // Insere linhas <tr> no body da tabela
 
     // Insere colunas <td> nas linhas <tr>
@@ -268,7 +275,85 @@ function getList(expense = [], filtred = false) {
     //   sessionStorage.setItem('update', el.id) // Salva o "id" da despesa que vai ser atualizada
     // }
    // row.insertCell(5).append(buttonDelete)
+    }
+  })
+}
 
+// Função que mostra a tabela das despesas
+function getListReceitas(expense = [], filtred = false) {
+  let tableExpenses = document.querySelector('#tableExpenses')
+  tableExpenses.innerHTML = '' // Limpando a tabela
+
+  if (expense.length == 0 && filtred === false) { // Caso não haja nenhuma solicitação de pesquisa
+    expense = db.getAllExpense()
+  }
+  else if (expense.length == 0 && filtred == true) { // Pesquisa solicitada, mas não encontrada
+    messageAlert('danger', 'Nenhuma despesa encontrada!')
+  }
+
+  expense.forEach(el => {
+// Testa se cada elemento campo filtro = "Receita"
+    if(el.filtro == "receita"){
+    let row = tableExpenses.insertRow() // Insere linhas <tr> no body da tabela
+
+    // Insere colunas <td> nas linhas <tr>
+    row.insertCell(0).innerHTML = `${el.day}`
+    // Tratativa do campo tipo
+    switch (el.type) {
+      case '1':
+        el.type = 'Salário'
+        break
+      case '2':
+        el.type = 'Adiantamento'
+        break
+      case '3':
+        el.type = '13º Salário'
+        break
+      case '4':
+        el.type = 'Aplicações'
+        break
+      case '5':
+        el.type = 'Outros'
+        break
+    }
+    row.insertCell(1).innerHTML = el.type
+    row.insertCell(2).innerHTML = el.description
+    row.insertCell(3).innerHTML = el.val
+
+    let buttonDelete = document.createElement('button')
+    buttonDelete.className = 'btn btn-danger'
+    buttonDelete.innerHTML = '<i class="fa fa-trash-o"></i>'
+    buttonDelete.onclick = () => { // Função que vai deletar despesa
+      db.delete(el.id)
+      messageAlert('success', 'Despesa excluída com sucesso!')
+      setTimeout(() => {
+        document.location.reload()
+      }, 3000)
+    }
+
+    let buttonUpdate = document.createElement('button')
+    buttonUpdate.className = 'btn btn-success '
+    buttonUpdate.id = 'update'
+    buttonUpdate.innerHTML = '<i class="fa fa-edit"</i>'
+    buttonUpdate.onclick = async () => {
+      await showExpenseModal(el.id) //  Aguarda o retorno dos valores para o modal
+      $('#modal').modal('show') // Abre o modal
+      sessionStorage.setItem('update', el.id) // Salva o "id" da despesa que vai ser atualizada
+    }
+
+    row.insertCell(4).append(buttonDelete, " ", buttonUpdate) // Alocando o elemento button na tabela
+
+    // let buttonUpdate = document.createElement('button')
+    // buttonUpdate.className = 'btn btn-success '
+    // buttonUpdate.id = 'update'
+    // buttonUpdate.innerHTML = '<i class="fa fa-edit"</i>'
+    // buttonUpdate.onclick = async () => {
+    //   await showExpenseModal(el.id) //  Aguarda o retorno dos valores para o modal
+    //   $('#modal').modal('show') // Abre o modal
+    //   sessionStorage.setItem('update', el.id) // Salva o "id" da despesa que vai ser atualizada
+    // }
+   // row.insertCell(5).append(buttonDelete)
+   }
   })
 }
 
@@ -293,9 +378,94 @@ function searchExpense() {
 
   let filtredExpenses = db.search(expense)
 
-  getList(filtredExpenses, true)
+  getListDespesas(filtredExpenses, true)
 
 }
+
+// Função que pesquisa as despesas
+function searchReceita() {
+  //let year = document.querySelector('#year') // Acessando os elementos do formulário
+  //let month = document.querySelector('#month')
+  let day = document.querySelector('#day')
+  let type = document.querySelector('#type')
+  let description = document.querySelector('#description')
+  let val = document.querySelector('#val')
+
+  let expense = new Expense(
+    id = '',
+    //year.value,
+    //month.value,
+    day.value,
+    type.value,
+    description.value,
+    val.value
+  )
+
+  let filtredExpenses = db.search(expense)
+
+  getListReceitas(filtredExpenses, true)
+
+}
+
+// Função que calcula receitas
+$( document ).ready(function somaReceitas() {
+
+  let soma_rc = 0
+  
+  let receita  = db.getAllExpense()
+  
+  receita.forEach(rc => {
+    if(rc.filtro == "receita"){
+      soma_rc += parseFloat(rc.val)
+    }
+  })
+  document.querySelector('#total_receitas').innerHTML = "R$ "+parseFloat(soma_rc)
+
+});
+
+// Função que calcula despesas
+$( document ).ready(function somaDespesas() {
+
+  let soma_dp = 0
+  
+  let despesa  = db.getAllExpense()
+  
+  despesa.forEach(dp => {
+    if(dp.filtro == "despesa"){
+      soma_dp += parseFloat(dp.val)
+    }
+  })
+  document.querySelector('#total_despesas').innerHTML = "R$ "+ parseFloat(soma_dp)
+
+});
+
+// Função que calcula balanço
+$( document ).ready(function balanco() {
+
+  let balanco = 0
+  
+  //somando receitas
+  let soma_rc = 0  
+  let receita  = db.getAllExpense() 
+  receita.forEach(rc => {
+    if(rc.filtro == "receita"){
+      soma_rc += parseFloat(rc.val)
+    }
+  })
+
+  //somando despesas
+  let soma_dp = 0
+  let despesa  = db.getAllExpense()
+  despesa.forEach(dp => {
+    if(dp.filtro == "despesa"){
+      soma_dp += parseFloat(dp.val)
+    }
+  })
+
+  //calculando balanço
+  balanco = parseFloat(soma_rc) - parseFloat(soma_dp)
+  document.querySelector('#balanco').innerHTML = "R$ "+ parseFloat(balanco)
+});
 
 // Função que traz os valores da despesa para dentro do modal
 async function showExpenseModal(id) {
@@ -320,10 +490,11 @@ async function updateExpense() {
   let type = document.querySelector('#mType')
   let description = document.querySelector('#mDescription')
   let val = document.querySelector('#mVal')
+  let filtro = document.querySelector('#mFiltro')
 
   let id = sessionStorage.getItem('update') // retorna o "id" da despesa que vai ser atualizada
   await db.update(id, //year.value, month.value,
-    day.value, type.value, description.value, val.value)
+    day.value, type.value, description.value, val.value, filtro.value)
 
   await $('#modal').modal('hide')
   // Precisamos mudar este comentário
